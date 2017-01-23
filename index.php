dd255eb9b0394591e1f8c31d5ec46a7cf36cf641
Juan camilo <jcholguins@unal.edu.co>
2017-01-23 16:15:02 -0500

    SEgundo comando

--- index.php
+++ index.php
@@ -1,12 +1,22 @@
 <?php 
 
     $i=0; 
-    $x=rand(0,20); 
+    $x=rand(0,10); 
             
     while($i<10)
     { 
         echo "<br>".$x." x ".$i." = ".$x*$i;
         $i++; 
-    } 
+    }
 
+    $a=1;
+    if($a) 
+    {
+    echo “Verdadero”;
+    } 
+    
+    else 
+    {
+    echo “Falso”;
+    }
 ?>
