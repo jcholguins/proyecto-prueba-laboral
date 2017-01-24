9f4b57ae48d4ce6e3115d18a1ffaa9c0eada3a36
Luis Felipe Tavera Orozco <lftavera@hotmail.com>
2017-01-23 21:04:46 -0500

    integrada copia de la base de datos

---
+++ AppData/reservas.sql
@@ -0,0 +1,159 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.4
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 24-01-2017 a las 03:04:00
+-- Versión del servidor: 10.1.16-MariaDB
+-- Versión de PHP: 7.0.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `reservas`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `clientes`
+--
+
+CREATE TABLE `clientes` (
+  `clie_id` int(11) NOT NULL,
+  `clie_nume_docu` int(11) NOT NULL,
+  `clie_nombre` varchar(25) NOT NULL,
+  `clie_email` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `clientes`
+--
+
+INSERT INTO `clientes` (`clie_id`, `clie_nume_docu`, `clie_nombre`, `clie_email`) VALUES
+(1, 213, 'sdf', 'sdf@sdf'),
+(2, 21312, 'sdes', 'weqwe'),
+(3, 123, 'wd', 'dsfsdf'),
+(4, 123, 'wd', 'dsfsdf'),
+(5, 23342, 'dsf', 'sdfsdf'),
+(6, 234, 'f', 'sdfsdf');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `reserva`
+--
+
+CREATE TABLE `reserva` (
+  `rese_id` int(11) NOT NULL,
+  `rese_clie_id` int(11) NOT NULL,
+  `rese_fecha` date NOT NULL,
+  `rese_valo_tota` int(11) NOT NULL,
+  `rese_observaciones` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `rese_deta`
+--
+
+CREATE TABLE `rese_deta` (
+  `rd_id` int(11) NOT NULL,
+  `rd_rese_id` int(11) NOT NULL,
+  `rd_cantidad` int(11) NOT NULL,
+  `rd_serv_id` int(11) NOT NULL,
+  `rd_valo_unit` int(11) NOT NULL,
+  `rd_valo_tota` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `servicios`
+--
+
+CREATE TABLE `servicios` (
+  `serv_id` int(11) NOT NULL,
+  `ser_nombre` varchar(25) NOT NULL,
+  `ser_descripcion` varchar(200) NOT NULL,
+  `ser_valo_serv` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `clientes`
+--
+ALTER TABLE `clientes`
+  ADD PRIMARY KEY (`clie_id`);
+
+--
+-- Indices de la tabla `reserva`
+--
+ALTER TABLE `reserva`
+  ADD PRIMARY KEY (`rese_id`),
+  ADD UNIQUE KEY `rese_clie_id` (`rese_clie_id`);
+
+--
+-- Indices de la tabla `rese_deta`
+--
+ALTER TABLE `rese_deta`
+  ADD PRIMARY KEY (`rd_id`),
+  ADD UNIQUE KEY `rd_rese_id` (`rd_rese_id`),
+  ADD UNIQUE KEY `rd_serv_id` (`rd_serv_id`);
+
+--
+-- Indices de la tabla `servicios`
+--
+ALTER TABLE `servicios`
+  ADD PRIMARY KEY (`serv_id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `clientes`
+--
+ALTER TABLE `clientes`
+  MODIFY `clie_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+--
+-- AUTO_INCREMENT de la tabla `rese_deta`
+--
+ALTER TABLE `rese_deta`
+  MODIFY `rd_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `servicios`
+--
+ALTER TABLE `servicios`
+  MODIFY `serv_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `reserva`
+--
+ALTER TABLE `reserva`
+  ADD CONSTRAINT `reserva_ibfk_1` FOREIGN KEY (`rese_clie_id`) REFERENCES `clientes` (`clie_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `rese_deta`
+--
+ALTER TABLE `rese_deta`
+  ADD CONSTRAINT `rese_deta_ibfk_1` FOREIGN KEY (`rd_rese_id`) REFERENCES `reserva` (`rese_id`),
+  ADD CONSTRAINT `rese_deta_ibfk_2` FOREIGN KEY (`rd_serv_id`) REFERENCES `servicios` (`serv_id`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
